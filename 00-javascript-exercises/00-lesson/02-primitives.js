659eb669274ecf08026b46ac67912d5bd15b61cb
Ernhoffer-Daniel <ernhofferdaniel1998@gmail.com>
2024-08-15 20:22:32 +0200

    0815

---
+++ 00-javascript-exercises/00-lesson/02-primitives.js
@@ -0,0 +1,55 @@
+// Js primitivek
+
+// Numerikus
+
+let age = 50;
+console.log(typeof age);
+
+let avgWeight = 74.5;
+console.log(avgWeight);
+
+
+console.log(Number.NAX_SAFE_INTEGER);
+
+
+// Szöveges vagy string
+
+let fullName = "Kovács Márk"
+console.log(fullName);
+let postCode = "1234"
+console.log(postCode);
+
+//  undefined
+let x;
+console.log(x);
+
+
+// null
+
+let emptyObj = null; 
+// kezdőérték
+
+// boolean
+
+let isActive = true;
+isActive = false;
+
+// Symbol---> unique, azaz egyedi értéket adok meg valaminek
+
+const syml1 = Symbol("is");
+const syml2 = Symbol("is");
+console.log("symbole are equal" , syml1 === syml2);
+
+// bigInt
+
+let numberOfGalaxies = 126516515615156165;
+console.log(numberOfGalaxies);
+
+
+
+
+
+
+
+
+
