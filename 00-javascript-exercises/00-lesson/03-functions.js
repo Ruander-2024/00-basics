2dce0a1aba6fabcb99d4b2aaf5482f01d32ff67b
Peter-Kohlrusz <peter.kohlrusz@gmail.com>
2024-08-22 18:33:20 +0200

    functions

---
+++ 00-javascript-exercises/00-lesson/03-functions.js
@@ -0,0 +1,29 @@
+// Függvények
+
+const sumOfAgesWomen = getSumAges(30, 28, 53);
+const sumOfAgesMen = getSumAges(73, 12, 34);
+
+console.log(sumOfAgesWomen);
+console.log(sumOfAgesMen);
+
+function getSumAges(age1, age2, age3){
+    // Függvény body
+    return age1 + age2 + age3; // return után nem kerül semmi lefuttatásra
+    let result = age1 + age2 + age3;
+    return result; // nem kell külön változókban eltárolni
+}
+
+console.log(getSumAges(11, 34, 23))
+
+const BASE_URL = "https";
+
+function sendDataToServer() {
+    function name() {
+        console.log(BASE_URL, "sent");  
+        console.log(`${BASE_URL}, "sent`);  
+    }
+    name();      
+        
+}
+
+sendDataToServer();
