03a40f52b7b37a45895b34d8e5236454a3b77dd1
Mazaine <mazaine89@gmail.com>
2024-08-22 17:46:32 +0200

    asd

---
+++ 00-javascript-exercises/00-lesson/01-var-let-const.js
@@ -0,0 +1,26 @@
+//változó var//
+console.log("------var változó------");
+
+var number = 20;
+console.log(number);
+
+//hoisting//
+var x;
+var x =30;
+console.log(x);
+
+//let változó//
+
+let letNumber = 21;
+console.log(letNumber);
+
+//const változó//
+const Web_Url = "http";
+console.log(Web_Url);
+
+//scope//
+console.log("------var változó------");
+
+function addNum (){
+    console.log(Web_Url);
+}
