9179fdc20d4a4ff6c1621e88f2d8c87d37a431aa
Mazaine <mazaine89@gmail.com>
2024-08-22 18:35:01 +0200

    function

--- 00-javascript-exercises/00-lesson/03_function/function.js
+++ 00-javascript-exercises/00-lesson/03_function/function.js
@@ -1,21 +1,28 @@
-// függvények
+// Függvények
 
-const sumOfAgesWoman = getSumAges(30, 28, 53);
-const sumOfAgesMan = getSumAges(73, 12, 34);
+const sumOfAgesWomen = getSumAges("30", "28", "53");
+const sumOfAgesMen = getSumAges(73, 12, 34);
 
-
-console.log (sumOfAgesMan);
-console.log (sumOfAgesWoman);
+console.log(sumOfAgesMen);
+console.log(sumOfAgesWomen);
 
 function getSumAges(age1, age2, age3){
-    //függvény body
-    return (age1+age2+age3)
+    // Függvény body
+    return age1 + age2 + age3; // return után nem kerül semmi lefutattásra
+    let result = age1 + age2 + age3;
+    return result; // nem kell külön változóban eltárolni
 }
 
-const Base_url = 'https'
- 
-function sendDataToServer ( ) {
-    function name( ) {
-        console.log(Base_url, "send") //ez hibás
+console.log(getSumAges(11, 34, 23));
+
+const BASE_URL = "https";
+
+function sendDataToServer() {
+    function name() {
+        console.log(BASE_URL, "sent");
+        console.log(${BASE_URL} sent);
     }
+    name();
 }
+
+sendDataToServer();
