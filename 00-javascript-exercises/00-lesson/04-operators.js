80615e3bb8508909bd4be8814f799957fad31f2d
Csorgo-Peter <peter.csorgo@icloud.com>
2024-08-27 17:26:24 +0200

    08.27

--- 00-javascript-exercises/00-lesson/04-operators.js
+++ 00-javascript-exercises/00-lesson/04-operators.js
@@ -78,3 +78,12 @@ console.log(result4);
 
 console.log(5 + 5 + "abc" + 5 + 5 + "def" + 5 + 5);
 //  10abc55def55
+
+let stringToNum = "123"
+console.log(stringToNum);
+console.log(Number(stringToNum));
+
+let stringToNum1 = "asd"
+console.log(stringToNum1);
+console.log(Number(stringToNum1));
+
