b57f0b6349ea6b3347713c2749cfdc062ead7a5c
skrivi91 <skrivanekistvan@gmail.com>
2024-08-27 14:23:46 +0200

    create new file

---
+++ 00-javascript-exercises/00-lesson/01-var-let-const.js
@@ -0,0 +1,55 @@
+// változó var 
+console.log(" ----- Var változó -----");
+
+
+var number = 20;
+
+console.log(number);
+
+// Hoisting
+
+var x;
+var x = 30;
+console.log(x);
+
+/// Let változó
+
+console.log(" ----- Let változó -----");
+
+let letNumber = 21;
+console.log(letNumber);
+
+// familyName;
+let familyName = "Kovács";
+console.log(familyName);
+//nem lehet először létrehozni és utána értéket adni, ez a var-nál lehetséges
+
+//Const változó
+
+console.log(" ----- Const változó -----");
+
+const WEB_URL = "http";
+// WEB_URL = "https" nem lehet megváltoztatni az értékét
+
+console.log(WEB_URL);
+
+// Scope - elérési terület
+
+console.log(" ----- Scope változó ----- ");
+
+function addNum(){
+  console.log(WEB_URL);
+  var localVar = 23;
+  console.log(localVar);
+  {
+  var blockVar = "blockVar"
+  let blockLet = "blockLet"
+  console.log(blockLet);
+  }
+  console.log(blockVar);
+  
+}
+
+// console.log(blockVar); kívülről nem tudjuk elérni, csak a függvényen belül
+
+addNum();
---
+++ 00-javascript-exercises/00-lesson/03-functions.js
@@ -0,0 +1,28 @@
+// Függvények
+
+const sumOfAgesWomen = getSumAges("30", "28", "53");
+const sumOfAgesMen = getSumAges(73, 12, 34);
+
+console.log(sumOfAgesWomen);
+console.log(sumOfAgesMen);
+
+function getSumAges(age1, age2, age3){
+    // Függvény body
+    return age1 + age2 + age3; // return után nem kerül semmi lefutattásra
+    let result = age1 + age2 + age3;
+    return result; // nem kell külön változóban eltárolni
+}
+
+console.log(getSumAges(11, 34, 23));
+
+const BASE_URL = "https";
+
+function sendDataToServer() {
+    function name() {
+        console.log(BASE_URL, "sent");
+        console.log(`${BASE_URL} sent`);
+    }
+    name();
+}
+
+sendDataToServer();
