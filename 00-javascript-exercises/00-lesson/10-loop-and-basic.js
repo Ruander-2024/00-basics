2ffaf0ec5ea4a30fe6377adeed76da896c38a9d3
Smiley1144 <kovacsm519@gmail.com>
2024-09-03 20:16:18 +0200

    for ciklus

---
+++ 00-javascript-exercises/00-lesson/10-loop-and-basic.js
@@ -0,0 +1,45 @@
+// for ciklusok
+
+for(let i = 0; i <= 5; i++) {
+    // console.log(i);
+    // 0 - 5
+}
+
+for (let i = 0; i < 5; i++) {
+    // console.log(i);
+    // 0 - 4
+}
+
+for (let i = 1; i <= 5; i++) {
+    // console.log(i);
+    // 1 - 5
+}
+
+for (let i = 0; i <= 5; i+=2) {
+    // console.log(i);
+    // 0, 2, 4
+}
+
+for (let i = 5; i >= 0; i--) {
+    // console.log(i);
+    // 5 - 0
+}
+
+// Tömbökkel
+
+const nums = [10, 23, 35, 46, 58];
+
+for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i]);
+    // 10, 23, 35, 46, 58
+}
+
+for (let i = nums.length - 1; i >= 0; i--) {
+    // console.log(nums[i]);
+    // 58, 46, 35, 23, 10
+}
+
+// for of - Akkor használjuk, amikor nincsen szükség az indexre, csak a tömbben található értékre
+for (const num of nums) {
+    console.log(num);
+}
